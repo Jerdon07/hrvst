f57f15e0626ca663924beac692448ade723ef186
Jerdon07 <jdlitaoen@gmail.com>
2026-09-14 20:50:42 +0800

    REFACTOR: Remove abstract method in the test case

--- tests/Unit/HandlesPostScheduleContractTest.php
+++ tests/Unit/HandlesPostScheduleContractTest.php
@@ -5,12 +5,6 @@ use App\Http\Controllers\Dealer\Schedule\DemandController;
 use App\Http\Controllers\Farmer\Schedule\SupplyController;
 use App\Http\Requests\Post\UpdatePostRequest;
 
-it('declares update() as abstract on the trait, forcing every consumer to implement it', function () {
-    $method = (new ReflectionClass(HandlesPostSchedule::class))->getMethod('update');
-
-    expect($method->isAbstract())->toBeTrue();
-});
-
 it('implements its own update() rather than silently inheriting a trait default', function (string $class) {
     $declaringClass = (new ReflectionClass($class))->getMethod('update')->getDeclaringClass()->getName();
 
