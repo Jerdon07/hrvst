8ad92212b7f10d4b411bb3e1683abecd0994c4ef
Jerdon07 <jdlitaoen@gmail.com>
2026-09-23 16:13:08 +0800

    REFACTOR: Run TS Lint

--- resources/js/components/shared/tables/DataTable.test.ts
+++ resources/js/components/shared/tables/DataTable.test.ts
@@ -1,5 +1,5 @@
-import { mount } from '@vue/test-utils'
 import type { ColumnDef } from '@tanstack/vue-table'
+import { mount } from '@vue/test-utils'
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import DataTable from '@/components/shared/tables/DataTable.vue'
 
